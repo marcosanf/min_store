a32937f586b8ff5234466d791f2caa957d46dda7
Marcos Chaves <marcosanf.c@gmail.com>
2023-08-23 21:05:34 -0300

    initial grid layout implementation

--- lib/features/products/presentation/widgets/product_list.dart
+++ lib/features/products/presentation/widgets/product_list.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:riverpod_store/features/products/data/product_repository.dart';
+import 'package:riverpod_store/features/products/presentation/widgets/product_tile.dart';
 
 class ProductList extends ConsumerWidget {
   const ProductList({super.key});
@@ -9,13 +11,20 @@ class ProductList extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final productAsync = ref.watch(fetchProductsProvider);
     return productAsync.when(
-      data: (products) => ListView.separated(
-        itemBuilder: (context, index) {
+      data: (products) => GridView.custom(
+        gridDelegate: SliverQuiltedGridDelegate(
+          crossAxisCount: 2,
+          repeatPattern: QuiltedGridRepeatPattern.inverted,
+          pattern: [const QuiltedGridTile(1, 1)],
+        ),
+        childrenDelegate: SliverChildBuilderDelegate((context, index) {
           final product = products[index];
-          return Text(product.title);
-        },
-        separatorBuilder: (context, index) => const Divider(),
-        itemCount: products.length,
+          return ProductTile(
+            title: product.title,
+            price: product.price.toString(),
+            image: product.image,
+          );
+        }),
       ),
       loading: () => const Center(child: CircularProgressIndicator()),
       error: (e, st) => Center(child: Text(e.toString())),
--- lib/features/products/presentation/widgets/product_tile.dart
+++ lib/features/products/presentation/widgets/product_tile.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
 
 class ProductTile extends StatelessWidget {
-  const ProductTile({super.key});
+  const ProductTile(
+      {super.key,
+      required this.title,
+      required this.price,
+      required this.image});
+  final String title;
+  final String price;
+  final String image;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        SizedBox(
+          height: 236,
+          width: 157,
+          child: Image.network(
+            image,
+            fit: BoxFit.fitHeight,
+          ),
+        ),
+      ],
+    );
   }
 }
